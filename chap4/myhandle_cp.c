8fd2a192fa9ed6ed8d6850c6ca3ca24d67f7a321
Ye-zixiao <1461018824@qq.com>
2020-07-24 14:20:54 +0800

    date-2020-07-24

---
+++ chap4/myhandle_cp.c
@@ -0,0 +1,63 @@
+﻿/*
+* 可以处理源文件中不想要的字符的拷贝程序
+* 默认情况下，使用myhandle_cp [srcfile] [tarfile] 会删除源文件中的空洞
+* 在加入第四个参数后，使用myhandle_cp [srcfile] [tarfile] [你不想要的字符] 会删除源文件中的你不想要的字符，
+* 然后拷贝到目标文件中
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<sys/stat.h>
+
+#define BUFSIZE 4096
+#define DEFAULT_MODE (S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH)
+
+/*
+* 输入输出缓冲区字符处理函数：
+* buf指向缓冲区，read_size表示前面的read函数读取了多少有效的字符放入到buf中，
+* ch表示希望从buf中删除的字符
+*/
+int buf_handle(char* buf, int read_size, char ch) {
+	int i;
+	for (i = 0; i < read_size; ++i) {
+		if (buf[i] == ch) {
+			int j = i + 1;
+			for (; j < read_size; ++j)
+				if (buf[j] != ch)
+					buf[i++] = buf[j];
+			break;
+		}
+	}
+	return i;
+}
+
+void err_sys(const char* msg) {
+	fprintf(stderr, msg);
+	exit(EXIT_FAILURE);
+}
+
+int main(int argc, char* argv[])
+{
+	if (argc < 3)
+		err_sys("usage:myhandle_cp [src file] [target file] [del_char]\n");
+	char ch = '\0';
+	if (argc == 4)ch = argv[3][0];
+
+	int val, src_fd, tar_fd;
+	char buf[BUFSIZE];
+	if ((src_fd = open(argv[1], O_RDONLY)) < 0 ||
+		(tar_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, DEFAULT_MODE)) < 0)
+		err_sys("open error\n");
+
+	while ((val = read(src_fd, buf, BUFSIZE)) > 0) {
+		val = buf_handle(buf, val, ch);
+		if (write(tar_fd, buf, val) != val)
+			err_sys("write error\n");
+	}
+	if (val < 0)
+		err_sys("read error\n");
+	exit(EXIT_SUCCESS);
+}
---
+++ chap4/mystat.c
@@ -0,0 +1,43 @@
+﻿/*
+* 使用stat而不是lstat函数检查指定路径文件的文件类型
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/stat.h>
+#include<sys/types.h>
+#include<unistd.h>
+
+void err_sys(const char* msg) {
+	fprintf(stderr, msg);
+	exit(EXIT_FAILURE);
+}
+
+int main(int argc, char* argv[])
+{
+	if (argc < 2)
+		err_sys("usage:mystat [pathname...]\n");
+
+	int i;
+	struct stat statbuf;
+	for (i = 1; i < argc; ++i) {
+		if (stat(argv[i], &statbuf)) {
+			fprintf(stderr, "%s stat error\n", argv[1]);
+			continue;
+		}
+
+		printf("%s: ", argv[i]);
+		switch (statbuf.st_mode & __S_IFMT) {
+		case __S_IFREG:printf("regular file\n"); break;
+		case __S_IFDIR:printf("directory\n"); break;
+		case __S_IFBLK:printf("block special\n"); break;
+		case __S_IFCHR:printf("character special\n"); break;
+		case __S_IFIFO:printf("fifo"); break;
+		case __S_IFLNK:printf("soft link\n"); break;
+		case __S_IFSOCK:printf("socket\n"); break;
+		default:printf("** unknown mode **\n"); break;
+		}
+	}
+	
+	exit(EXIT_SUCCESS);
+}
